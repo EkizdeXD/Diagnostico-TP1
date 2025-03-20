a1046307637e631687dd2f5bf546c16d39f31a01
Ekizde <alexisamena4@gmail.com>
2025-03-19 21:49:53 -0300

    Correccion de errores + finalizacion del punto 8

--- 12-3/Program.cs
+++ 12-3/Program.cs
@@ -115,6 +115,63 @@ namespace _12_3
                     } while (N == 1 || N == 2 || N == 3 || N == 4 || N == 5 || N == 6 || N == 7);
                     break;
                 case 8:
+                    do
+                    {
+                        Console.WriteLine("Ingrese Un Numero Del 1 Al 12");
+                        N = int.Parse(Console.ReadLine());
+                        if (N == 1)
+                        {
+                            Console.WriteLine("Enero");
+                        }
+                        else if (N == 2)
+                        {
+                            Console.WriteLine("Febrero");
+                        }
+                        else if (N == 3)
+                        {
+                            Console.WriteLine("Marzo");
+                        }
+                        else if (N == 4)
+                        {
+                            Console.WriteLine("Abril");
+                        }
+                        else if (N == 5)
+                        {
+                            Console.WriteLine("Mayo");
+                        }
+                        else if (N == 6)
+                        {
+                            Console.WriteLine("Junio");
+                        }
+                        else if (N == 7)
+                        {
+                            Console.WriteLine("Julio");
+                        }
+                        else if (N == 8)
+                        {
+                            Console.WriteLine("Agosto");
+                        }
+                        else if (N == 9)
+                        {
+                            Console.WriteLine("Septiembre");
+                        }
+                        else if (N == 10)
+                        {
+                            Console.WriteLine("Octubre");
+                        }
+                        else if (N == 11)
+                        {
+                            Console.WriteLine("Noviembre");
+                        }
+                        else if (N == 12)
+                        {
+                            Console.WriteLine("Diciembre");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: Seleccione un numero valido");
+                        }
+                    } while (N == 1 || N == 2 || N == 3 || N == 4 || N == 5 || N == 6 || N == 7 || N == 8 || N == 9 || N == 10 || N == 11 || N == 12);
                     break;
                 case 9:
                     break;
